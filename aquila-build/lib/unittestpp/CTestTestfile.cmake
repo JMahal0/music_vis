428c59ba127c0fac1853f24b911f338029687d61
JMahal0 <jmahal@berkeley.edu>
2016-04-29 03:04:57 -0700

    Deleted unnecessary build folder

--- aquila-build/lib/unittestpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Desktop/music_vis/aquila-src/lib/unittestpp
-# Build directory: /home/ubuntu/Desktop/music_vis/aquila-build/lib/unittestpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(TestUnitTest++ "/home/ubuntu/Desktop/music_vis/aquila-build/lib/unittestpp/TestUnitTest++")
