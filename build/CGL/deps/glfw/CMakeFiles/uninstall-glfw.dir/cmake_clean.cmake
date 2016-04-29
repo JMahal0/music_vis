11c6444ade9e45863a448a53f9f181f1074d9961
JMahal0 <jmahal@berkeley.edu>
2016-04-29 03:03:12 -0700

    Correctly linked Aquila library

---
+++ build/CGL/deps/glfw/CMakeFiles/uninstall-glfw.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/uninstall-glfw"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/uninstall-glfw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
